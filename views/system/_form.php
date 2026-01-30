554939794273751cf3786c9b3d61efcfe0326944
Aleksandr Tochilin <alex8079@gmail.com>
2026-01-30 10:42:12 +0300

    refactor

--- views/system/_form.php
+++ views/system/_form.php
@@ -8,45 +8,35 @@ use yii\bootstrap5\ActiveForm;
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
-<div class="system-form row">
-    <div class="col-md-10 col-lg-8 col-xl-6">
+<div class="system-form d-inline-block">
+    <?php $form = ActiveForm::begin([
+        'enableClientValidation' => false,
+        'enableClientScript' => false,
+        'fieldConfig' => [
+            'labelOptions' => ['class' => 'col-form-label text-nowrap me-2'],
+            'options' => ['class' => 'mb-3 d-flex']
+        ]
+    ]); ?>
 
-        <?php $form = ActiveForm::begin(['enableClientValidation' => false, 'enableClientScript' => false]); ?>
-        <div class="row">
-            <div class="col-md-6">
-                <?= $form->field($model, 'name')->textInput(['required' => true, 'minLength' => 3]) ?>
+    <?= $form->field($model, 'name')->textInput(/* ['required' => true, 'minLength' => 3] */) ?>
 
-                <?= $form
-                    ->field($model, 'primary_economy')
-                    ->dropDownList(economies(), ['prompt' => 'select economy']) ?>
+    <?= $form->field($model, 'primary_economy')->dropDownList(economies(), ['prompt' => 'select economy']) ?>
 
-                <?= $form
-                    ->field($model, 'secondary_economy')
-                    ->dropDownList(economies(), ['prompt' => 'select economy']) ?>
+    <?= $form->field($model, 'secondary_economy')->dropDownList(economies(), ['prompt' => 'select economy']) ?>
 
-                <?= $form
-                    ->field($model, 'government')
-                    ->dropDownList(governments(), ['prompt' => 'select government']) ?>
-            </div>
+    <?= $form->field($model, 'government')->dropDownList(governments(), ['prompt' => 'select government']) ?>
 
-            <div class="col-md-6">
-                <?= $form
-                    ->field($model, 'allegiance')
-                    ->dropDownList(allegiances(), ['prompt' => 'select allegiance']) ?>
+    <?= $form->field($model, 'allegiance')->dropDownList(allegiances(), ['prompt' => 'select allegiance']) ?>
 
-                <?= $form->field($model, 'security')->dropDownList(sec_levels(), ['prompt' => 'select security']) ?>
+    <?= $form->field($model, 'security')->dropDownList(sec_levels(), ['prompt' => 'select security']) ?>
 
-                <?= $form->field($model, 'population')->input('number') ?>
+    <?= $form->field($model, 'population')->input('number') ?>
 
-                <?= $form->field($model, 'star_type')->textInput() ?>
-            </div>
-        </div>
+    <?= $form->field($model, 'star_type')->textInput() ?>
 
-        <div class="mb-3">
-            <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
-        </div>
-
-        <?php ActiveForm::end(); ?>
+    <div class="mb-3">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
 
+    <?php ActiveForm::end(); ?>
 </div>
