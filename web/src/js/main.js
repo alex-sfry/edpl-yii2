7360ea3c5b26ad020a3996925b2b0899e6236c00
Aleksandr Tochilin <alex8079@gmail.com>
2026-02-01 04:22:34 +0300

    comment out some unused stuff

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -26,29 +26,29 @@ function datalist(url) {
     
 }
 
-function createMaterialInputs() {
-    $('#add-material').on('click', function () {
-        const qty = $('.material-cnt').length;
-        console.log(qty)
-        $('.material-cnt').last().clone().appendTo("#materials-controls-cnt");
+// function createMaterialInputs() {
+//     $('#add-material').on('click', function () {
+//         const qty = $('.material-cnt').length;
+//         console.log(qty)
+//         $('.material-cnt').last().clone().appendTo("#materials-controls-cnt");
         
 
-        const $label = $('.mat-label').last();
-        const $select = $('.mat-control').last();
-        const $input = $('.percentage-control').last();
+//         const $label = $('.mat-label').last();
+//         const $select = $('.mat-control').last();
+//         const $input = $('.percentage-control').last();
 
-        $select.attr('id', $select.attr('id').replace(`-${qty -1}`, `-${qty}`));
-        $label.attr('for', $label.attr('for').replace(`-${qty - 1}`, `-${qty}`));
-        $input.attr('id', $input.attr('id').replace(`-${qty - 1}`, `-${qty}`));
-        $input.val('');
-    })
+//         $select.attr('id', $select.attr('id').replace(`-${qty -1}`, `-${qty}`));
+//         $label.attr('for', $label.attr('for').replace(`-${qty - 1}`, `-${qty}`));
+//         $input.attr('id', $input.attr('id').replace(`-${qty - 1}`, `-${qty}`));
+//         $input.val('');
+//     })
 
-    $('#remove-material').on('click', function () {
-        if ($('.material-cnt').length > 1) $('.material-cnt').last().remove();
-    })
-}
+//     $('#remove-material').on('click', function () {
+//         if ($('.material-cnt').length > 1) $('.material-cnt').last().remove();
+//     })
+// }
 
 // if (document.getElementById('station-system_id')) comboBox('station-system_id', '/systems/search?q=');
 // if (document.getElementById('planet-system_id')) comboBox('planet-system_id', '/systems/search?q=');
 // if (document.getElementById('select-planet_id')) comboBox('select-planet_id', '/planets/search?q=');
-if (document.getElementById('planet-material-form')) createMaterialInputs();
+// if (document.getElementById('planet-material-form')) createMaterialInputs();
