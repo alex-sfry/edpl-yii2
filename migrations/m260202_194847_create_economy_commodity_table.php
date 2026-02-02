96ebffccc7362d6f3ee94c667f72ab85780076e6
Aleksandr Tochilin <alex8079@gmail.com>
2026-02-02 22:54:12 +0300

    add create_economy_commodity migration

---
+++ migrations/m260202_194847_create_economy_commodity_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%economy_commodity}}`.
+ */
+class m260202_194847_create_economy_commodity_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%economy_commodity}}', [
+            'id' => $this->primaryKey(),
+            'commodity_id' => $this->integer()->notNull(),
+            'economy' => $this->string(50)->notNull()->append('COLLATE NOCASE'),
+            'trade_type' => $this->string(50)->notNull()->append('COLLATE NOCASE'),
+            'created_at' =>  $this->integer()->notNull(),
+            'updated_at' =>  $this->integer()->notNull(),
+        ]);
+        $this->createIndex(
+            'economy_commodity_commodity_id_economy_trade_type_idx',
+            'economy_commodity',
+            ['commodity_id', 'economy', 'trade_type'],
+            true
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%economy_commodity}}');
+    }
+}
