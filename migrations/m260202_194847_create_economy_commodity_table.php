71baf12b0fb0a7a552416742769f8df41e5439fd
Aleksandr Tochilin <alex8079@gmail.com>
2026-02-06 03:47:46 +0300

    modify create_economy_commodity_table migration

--- migrations/m260202_194847_create_economy_commodity_table.php
+++ migrations/m260202_194847_create_economy_commodity_table.php
@@ -14,16 +14,17 @@ class m260202_194847_create_economy_commodity_table extends Migration
     {
         $this->createTable('{{%economy_commodity}}', [
             'id' => $this->primaryKey(),
-            'commodity_id' => $this->integer()->notNull(),
+            'commodity' => $this->string(255)->notNull()->append('COLLATE NOCASE'),
+            'category' => $this->string(50)->notNull()->append('COLLATE NOCASE'),
             'economy' => $this->string(50)->notNull()->append('COLLATE NOCASE'),
             'trade_type' => $this->string(50)->notNull()->append('COLLATE NOCASE'),
             'created_at' =>  $this->integer()->notNull(),
             'updated_at' =>  $this->integer()->notNull(),
         ]);
         $this->createIndex(
-            'economy_commodity_commodity_id_economy_trade_type_idx',
+            'economy_commodity_commodity_economy_trade_type_idx',
             'economy_commodity',
-            ['commodity_id', 'economy', 'trade_type'],
+            ['commodity', 'economy', 'trade_type'],
             true
         );
     }
