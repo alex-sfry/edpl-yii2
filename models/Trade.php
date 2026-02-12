8b5ac68086d9ecee0a9e0475c1e3d59bef8d647d
Aleksandr Tochilin <alex8079@gmail.com>
2026-02-12 07:10:09 +0300

    add Trade model

---
+++ models/Trade.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * This is the model class for table "trades".
+ *
+ * @property int $id
+ * @property int $commodity_id
+ * @property int $buy_station_id
+ * @property int $buy_price
+ * @property int $sell_station_id
+ * @property int $sell_price
+ * @property int $created_at
+ * @property int $updated_at
+ */
+class Trade extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::class,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'trades';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['commodity_id', 'buy_station_id', 'buy_price', 'sell_station_id', 'sell_price'], 'required'],
+            [['commodity_id', 'buy_station_id', 'buy_price', 'sell_station_id', 'sell_price'], 'integer'],
+            [
+                ['buy_station_id'],
+                'exist',
+                'targetClass' => Station::class,
+                'targetAttribute' => ['buy_station_id' => 'id'],
+                'message' => "Station doesn't exist."
+            ],
+            [
+                ['sell_station_id'],
+                'exist',
+                'targetClass' => Station::class,
+                'targetAttribute' => ['sell_station_id' => 'id'],
+                'message' => "Station doesn't exist."
+            ],
+            [
+                ['commodity_id'],
+                'exist',
+                'targetClass' => Commodity::class,
+                'targetAttribute' => ['commodity_id' => 'id'],
+                'message' => "Commodity doesn't exist."
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'commodity_id' => 'Commodity ID',
+            'buy_station_id' => 'Buy Station ID',
+            'buy_price' => 'Buy Price',
+            'sell_station_id' => 'Sell Station ID',
+            'sell_price' => 'Sell Price',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
