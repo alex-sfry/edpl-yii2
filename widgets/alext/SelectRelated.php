44350c4e5087ed74726d5c149a461ba5fe445728
Aleksandr Tochilin <alex8079@gmail.com>
2026-02-12 10:47:58 +0300

    make widget suitable for selecting values from any related table, add phpdoc

--- widgets/alext/SelectRelated.php
+++ widgets/alext/SelectRelated.php
@@ -8,10 +8,37 @@ use yii\helpers\Url;
 
 class SelectRelated extends \yii\bootstrap5\Widget
 {
-    public \yii\bootstrap5\ActiveForm|\yii\widgets\ActiveForm $form;
-    public mixed $model;
-    public string $attribute;
-    public string $type = 'text';
+    /**
+     * @var \yii\bootstrap5\ActiveForm
+     */
+    public $form;
+
+    /**
+     * @var \yii\db\ActiveRecord
+     */
+    public $model;
+
+    /**
+     * @var string
+     */
+    public $attribute;
+
+    /**
+     * @var string
+     */
+    public $type = 'text';
+
+    /**
+     * Route for Url helper - page to go after selection is made
+     * @var string
+     */
+    public $return;
+
+    /**
+     * Route for Url helper - page where selection is made
+     * @var string
+     */
+    public $select;
 
     /**
      * {@inheritdoc}
@@ -20,25 +47,26 @@ class SelectRelated extends \yii\bootstrap5\Widget
     {
         $html = '';
 
-        $html .= $this->form->beginField($this->model, 'system_id', ['options' => ['class' => 'mb-3']]);
+        $html .= $this->form->beginField($this->model, $this->attribute, ['options' => ['class' => 'mb-3']]);
         $html .= '<div class="d-flex">';
-        $html .= Html::activeLabel($this->model, 'system_id', ['class' => 'col-form-label text-nowrap me-2']); 
+        $html .= Html::activeLabel($this->model, $this->attribute, ['class' => 'col-form-label text-nowrap me-2']);
         $html .= Html::activeInput(
             $this->type,
             $this->model,
-            'system_id', ['class' => 'form-control', 'readonly' => true]
+            $this->attribute,
+            ['class' => 'form-control', 'readonly' => true]
         );
 
         $href = Url::to(["/system/select", "return" => Url::to(["/station/create"])]);
         $html .= '<a class="btn btn-secondary ms-1 text-nowrap" href="' . $href . '">select system</a>';
         $html .= '</div>';
-        
+
         if ($this->model->hasErrors('system_id')) {
             $html .= '<div class="mt-1 text-danger">';
-                $html .= '<small>' . e($this->model->getFirstError('system_id')) . '</small>';
+            $html .= '<small>' . e($this->model->getFirstError('system_id')) . '</small>';
             $html .= '</div>';
         }
-        
+
         $html .= $this->form->endField();
 
         return $html;
